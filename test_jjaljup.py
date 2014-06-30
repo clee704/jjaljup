e2e96fea7724e59b5b04247034b5328ccc67db69
Choongmin Lee <choongmin@me.com>
2014-06-30 23:17:52 +0900

    Add tests

--- test_jjaljup.py
+++ test_jjaljup.py
@@ -1,8 +1,24 @@
 from urlparse import urlparse
 
-from jjaljup import get_twitter_agif
+from jjaljup import get_twitpic, get_twitter_agif, get_yfrog
+
 
 def test_get_twitter_agif():
     url = 'https://twitter.com/verge/status/479306079202209792/photo/1'
     video_url = 'https://pbs.twimg.com/tweet_video/BqbWVjQIAAEpReg.mp4'
     assert get_twitter_agif(url, urlparse(url)).url == video_url
+
+
+def test_get_twitpic():
+    url = 'http://twitpic.com/dhyrtq'
+    image_url = 'http://twitpic.com/show/full/dhyrtq'
+    name = '816236126.jpg'
+    img = get_twitpic(url, urlparse(url))
+    assert img.url == image_url
+    assert img.name == name
+
+
+def test_get_yfrog():
+    url = 'http://yfrog.com/od4hocllj'
+    image_url = 'http://a.yfrog.com/img877/9894/4hocll.jpg'
+    assert get_yfrog(url, urlparse(url)).url == image_url
